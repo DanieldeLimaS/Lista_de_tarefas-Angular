290d8bb0fe45e050b725d2a733c841952473b777
danielsollus <danielsuporteti@outlook.com.br>
2022-03-16 17:37:41 -0300

    inicial

---
+++ src/Models/todo.models.ts
@@ -0,0 +1,8 @@
+export class Todo{
+    constructor(
+        public id:Number=1,
+        public title:String,
+        public done:Boolean
+    ){}
+   
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/Models/todo.models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title:String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1,'Fazer compras',false));
+    this.todos.push(new Todo(2,'Cortar o cabelo',false));
+    this.todos.push(new Todo(3,'Tirar o lixo',false));
+    this.todos.push(new Todo(4,'lavar banheiro',false));
+  }
+  alteraTexto(){
+    this.title='Teste';
+  }
 }
